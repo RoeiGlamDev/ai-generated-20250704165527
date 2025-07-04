437101c30ba354c6f1a45ee70b2874f1b9a06f6c
RoeiGlamDev <Roeiglam2@gmail.com>
2025-07-04 23:55:29 +0700

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // Sentry configuration
+  sentry: {
+    // Your Sentry project name
+    projectName: 'javascript-nextjs',
+    // Your Sentry organization name
+    organizationName: 'codeforge-ai',
+  },
+}
