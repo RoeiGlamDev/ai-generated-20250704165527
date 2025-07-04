dfe8923119f5bac0a942b489b458f149da151c7b
RoeiGlamDev <Roeiglam2@gmail.com>
2025-07-04 23:55:30 +0700

    Add sentry.server.config.js

---
+++ sentry.server.config.js
@@ -0,0 +1,4 @@
+// Sentry server configuration
+export const SENTRY_DSN = process.env.SENTRY_DSN;
+export const SENTRY_ENV = process.env.SENTRY_ENV;
+export const SENTRY_RELEASE = process.env.SENTRY_RELEASE;
